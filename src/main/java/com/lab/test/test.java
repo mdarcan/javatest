654763a8a622a2a38152d3a18d36f8a9222b7002
mdarcan <massimo.darcangelo@tiscali.it>
2017-10-10 18:43:49 +0200

    quello buono

---
+++ src/main/java/com/lab/test/test.java
@@ -0,0 +1,18 @@
+package com.lab.test;
+
+import com.lab.main.REBotLogin;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		REBotLogin tt = new REBotLogin();
+		tt.MainThread();
+	}
+
+}
